57e716029af541ca7b4545ba9818c6f18076de05
James La Marre <jameslamarre@gmail.com>
2023-09-28 13:45:48 -0700

    feat: tooltip dialog

--- src/components/sanity/tooltip/SanityTooltip.tsx
+++ src/components/sanity/tooltip/SanityTooltip.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { Dialog } from '@headlessui/react'
 import { PlainText } from '@studio/gen/sanity-schema'
 import { useState, type FC, type HTMLAttributes } from 'react'
 import { RichText } from '../rich-text'
 import { IconX } from '@components/icons'
+import { Dialog } from '@headlessui/react'
 
 interface SanityTooltipCloseProps extends HTMLAttributes<HTMLElement> {
   onClose?: () => void
@@ -33,7 +33,7 @@ export const SanityTooltip: FC<SanityTooltipProps> = ({
     return (
       <>
         <a
-          aria-label={`Open Map`}
+          aria-label={`Open tooltip`}
           onClick={() => setIsOpen(true)}
           className="inline hover:font-bold border-bottom"
         >
@@ -51,6 +51,33 @@ export const SanityTooltip: FC<SanityTooltipProps> = ({
           </Dialog.Panel>
         </Dialog>
       </>
+
+      // leaving here in case we need to use chakra (needs provider) ~ JLM
+      // <Popover
+      //   isOpen={isOpen}
+      //   onClose={() => setIsOpen(false)}
+      //   trigger="click"
+      //   usePortal={true}
+      //   gutter={10}
+      //   placement="top-end"
+      // >
+      //   <PopoverTrigger>
+      //     <a
+      //       aria-label={`Open tooltip`}
+      //       onClick={() => setIsOpen(true)}
+      //       className="inline hover:font-bold border-bottom"
+      //     >
+      //       {props.linkedCopy}
+      //     </a>
+      //   </PopoverTrigger>
+      //   <PopoverContent
+      //     bg="transparent"
+      //     className="relative max-w-[672px] w-full top-[72px] left-2 z-modal"
+      //   >
+      //     <CloseButton onClose={() => setIsOpen(false)} />
+      //     <RichText blocks={props.tooltipContent} className={'p-x'} />
+      //   </PopoverContent>
+      // </Popover>
     )
 }
 
