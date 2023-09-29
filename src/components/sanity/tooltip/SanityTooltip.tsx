f82456ff88903a910ada84a93346f5651e786a9a
James La Marre <jameslamarre@gmail.com>
2023-09-28 17:16:05 -0700

    fix: tooltip finished

--- src/components/sanity/tooltip/SanityTooltip.tsx
+++ src/components/sanity/tooltip/SanityTooltip.tsx
@@ -3,7 +3,12 @@ import { PlainText } from '@studio/gen/sanity-schema'
 import { useState, type FC, type HTMLAttributes } from 'react'
 import { RichText } from '../rich-text'
 import { IconX } from '@components/icons'
-import { Dialog } from '@headlessui/react'
+import {
+  Portal,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from '@chakra-ui/react'
 
 interface SanityTooltipCloseProps extends HTMLAttributes<HTMLElement> {
   onClose?: () => void
@@ -31,53 +36,34 @@ export const SanityTooltip: FC<SanityTooltipProps> = ({
   let [isOpen, setIsOpen] = useState(false)
   if (props.tooltipContent)
     return (
-      <>
-        <a
-          aria-label={`Open tooltip`}
-          onClick={() => setIsOpen(true)}
-          className="inline hover:font-bold border-bottom"
-        >
-          {props.linkedCopy}
-        </a>
-
-        {/* <Dialog
-          open={isOpen}
-          onClose={() => setIsOpen(false)}
-          className="relative max-w-[672px] w-full top-[72px] left-2 z-modal"
-        >
-          <Dialog.Panel className="block relative border-1 border-solid border-black w-full h-full bg-white">
+      <Popover
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        trigger="click"
+        placement="top-end"
+      >
+        <PopoverTrigger>
+          <a
+            aria-label={`Open tooltip`}
+            onClick={() => setIsOpen(true)}
+            className="inline hover:font-bold border-bottom"
+          >
+            {props.linkedCopy}
+          </a>
+        </PopoverTrigger>
+        <Portal>
+          <PopoverContent
+            variants={{}}
+            className="relative max-w-[calc(100vw-2rem)] md:max-w-[672px] w-full border-1 border-black border-solid left-x md:left-2 bg-white z-modal"
+          >
             <CloseButton onClose={() => setIsOpen(false)} />
-            <RichText blocks={props.tooltipContent} className={'p-x'} />
-          </Dialog.Panel>
-        </Dialog> */}
-      </>
-
-      // leaving here in case we need to use chakra (needs provider) ~ JLM
-      // <Popover
-      //   isOpen={isOpen}
-      //   onClose={() => setIsOpen(false)}
-      //   trigger="click"
-      //   usePortal={true}
-      //   gutter={10}
-      //   placement="top-end"
-      // >
-      //   <PopoverTrigger>
-      //     <a
-      //       aria-label={`Open tooltip`}
-      //       onClick={() => setIsOpen(true)}
-      //       className="inline hover:font-bold border-bottom"
-      //     >
-      //       {props.linkedCopy}
-      //     </a>
-      //   </PopoverTrigger>
-      //   <PopoverContent
-      //     bg="transparent"
-      //     className="relative max-w-[672px] w-full top-[72px] left-2 z-modal"
-      //   >
-      //     <CloseButton onClose={() => setIsOpen(false)} />
-      //     <RichText blocks={props.tooltipContent} className={'p-x'} />
-      //   </PopoverContent>
-      // </Popover>
+            <RichText
+              blocks={props.tooltipContent}
+              className={'bg-white p-[40px] md:p-x'}
+            />
+          </PopoverContent>
+        </Portal>
+      </Popover>
     )
 }
 
