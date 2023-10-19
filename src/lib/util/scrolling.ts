9ae79dda901d70c559825c8df0e9c7a2f9e78e79
YannyD <yan@earth.net>
2023-10-19 15:51:17 -0500

    fix: scroll image offset

--- src/lib/util/scrolling.ts
+++ src/lib/util/scrolling.ts
@@ -1,12 +1,10 @@
 import { animateScroll as scroll } from 'react-scroll'
 
-export const scrollToEl = (el: HTMLElement) => {
+export const scrollToEl = (el: HTMLElement, tile?: boolean) => {
   if (typeof window !== 'undefined') {
-    const offset = window.innerWidth < 768 ? 16 : 40
+    const offset = window.innerWidth < 768 ? 46 : tile ? -5 : -46
 
     const top = el.getBoundingClientRect().top + window.scrollY - offset
     scroll.scrollTo(top)
   }
 }
-
-export default scrollToEl
