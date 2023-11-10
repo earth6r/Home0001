b4ac4b8cd4270fddaf40aa93e1808f3c56a5de42
YannyD <yan@earth.net>
2023-11-10 11:38:48 -0600

    fix: property unit link

--- src/components/unit/UnitSummary.tsx
+++ src/components/unit/UnitSummary.tsx
@@ -8,7 +8,8 @@ import { sendGoogleEvent, sendHubspotEvent } from '@lib/util'
 import { SanityLink, SanityMedia } from '@components/sanity'
 import { SanityLinkType } from '@studio/lib'
 import Link from 'next/link'
-
+import { ImageCarousel } from '@components/carousel'
+import { redirect } from 'next/navigation'
 export const UnitSummary: FC<UnitListProps> = ({ unit }) => {
   const router = useRouter()
   const { dispatch, state } = useContext(HomeContext)
@@ -46,11 +47,11 @@ export const UnitSummary: FC<UnitListProps> = ({ unit }) => {
   }
 
   if (!unit) return null
+  const summaryPhotos = unit?.photographs?.slice(0, 4)
+
   return (
     <li>
-      <Link
-        href={`/unit/${unit.slug?.current}`}
-        onClick={() => updateUnit(unit, unit.title)}
+      <div
         className={classNames(
           unit.available ? '' : 'bg-white shadow-none opacity-30',
           `transition-colors   px-4 pt-4 pb-0 min-h-[16rem] w-full grid justify-stretch flex-col`
@@ -60,10 +61,12 @@ export const UnitSummary: FC<UnitListProps> = ({ unit }) => {
           <p className="col-start-1 text-left uppercase">
             {unit.title && <span>{unit.title}</span>}
           </p>
-          <p className="col-start-2 text-left">{unit.price}</p>
+          <p className="md:col-start-1 col-start-2 md:col-span-1 text-left">
+            {unit.price}
+          </p>
         </div>
         <div className="mb-5 z-above">
-          <SanityMedia
+          {/* <SanityMedia
             image={unit.headlineImage?.image as any}
             imageProps={{
               alt: unit.headlineImage?.alt || 'Home0001 Headline Image',
@@ -71,36 +74,55 @@ export const UnitSummary: FC<UnitListProps> = ({ unit }) => {
               lqip: (unit.headlineImage?.image as any)?.asset?.metadata.lqip,
               quality: 8,
             }}
-          />
-        </div>
-        <div className="justify-self-stretch w-full ">
-          <div className="mt-4 mb-2 text-left rich-text">
-            {unit.area && (
-              <p className="mb-5">
-                {unit.area}
-                <br />
-                Fully equipped
-                <br />
-                Access to homes in other locations
-              </p>
+          /> */}
+          <div className="flex flex-col relative mt-10">
+            {unit?.photographs && unit?.photographs.length > 0 && (
+              <ImageCarousel
+                index="0"
+                slides={summaryPhotos}
+                className="md:max-w-[400px] max-w-[50%]"
+              />
             )}
+            <div className="justify-self-stretch w-full ">
+              <div className="mt-4 mb-2 text-left rich-text">
+                {unit.area && (
+                  <p className="mb-5">
+                    {unit.area}
+                    <br />
+                    Fully equipped
+                    <br />
+                    Access to homes in other locations
+                  </p>
+                )}
+              </div>
+              <Link
+                href={`/unit/${unit.slug?.current}`}
+                onClick={() => {
+                  updateUnit(unit, unit.title)
+                }}
+              >
+                {unit.slug && (
+                  <button
+                    className={classNames(
+                      `relative border-1 border-black border-solid mb-[2px] flex flex-row justify-between items-center w-full h-12 max-h-12 hover:bg-black hover:text-white bg-white z-above`
+                    )}
+                    onClick={() => {
+                      updateUnit(unit, unit.title)
+                    }}
+                  >
+                    <span className="mb-0 py-2 text-left pl-4 uppercase">
+                      {`Explore ${unit?.title}`}
+                    </span>
+                    <span className="py-2 pb-[0.55rem] text-[16px] text-right pr-4">
+                      →
+                    </span>
+                  </button>
+                )}
+              </Link>
+            </div>
           </div>
-          {unit.slug && (
-            <button
-              className={classNames(
-                `relative border-1 border-black border-solid mb-[2px] flex flex-row justify-between items-center w-full h-12 max-h-12 hover:bg-black hover:text-white bg-white z-above`
-              )}
-            >
-              <span className="mb-0 py-2 text-left pl-4 uppercase">
-                {`Explore ${unit?.title}`}
-              </span>
-              <span className="py-2 pb-[0.55rem] text-[16px] text-right pr-4">
-                →
-              </span>
-            </button>
-          )}
         </div>
-      </Link>
+      </div>
     </li>
   )
 }
