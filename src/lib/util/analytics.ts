6ab27529c2f9999d4e02b57d0e3387286f3d5614
YannyD <yan@earth.net>
2023-11-22 09:07:36 -0600

    fix: define test events outside of production

--- src/lib/util/analytics.ts
+++ src/lib/util/analytics.ts
@@ -4,8 +4,11 @@ const GOOGLE_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_ID
 export const sendGoogleEvent = (event: string, options?: {}) => {
   if (typeof window !== 'undefined' && GOOGLE_ID) {
     window.dataLayer = window.dataLayer || []
+    const isProductionSite =
+      window?.location?.origin === 'https://www.home0001.com'
+
     window.dataLayer.push({
-      event: event,
+      event: isProductionSite ? event : 'test_' + event,
       options,
     })
     window['google_tag_manager'][GOOGLE_ID].dataLayer.reset()
