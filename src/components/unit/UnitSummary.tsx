abc88586c8a4f1fa68330748b70168532700ed9b
m00nface <hafeezdawood@gmail.com>
2023-11-23 03:55:45 +0000

    fix: margin carousel

--- src/components/unit/UnitSummary.tsx
+++ src/components/unit/UnitSummary.tsx
@@ -77,7 +77,7 @@ export const UnitSummary: FC<UnitListProps> = ({ unit, border, className }) => {
                 index="0"
                 slides={unit?.photographs}
                 carousel={true}
-                className="md:max-w-[346px] mb-4"
+                className="md:max-w-[346px] mb-3"
               />
             )}
             <div className="block w-auto max-w-[467px] bg-darkgray py-x pl-x mr-4 md:mr-0 pr-menu">
