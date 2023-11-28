6bc2ad75f5fb88fb0645eb3c5fabf5cac907f2b0
James La Marre <jameslamarre@gmail.com>
2023-11-28 15:00:46 -0800

    fix: homepage useeffect check

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { ForwardRefRenderFunction, forwardRef } from 'react'
+import { ForwardRefRenderFunction, forwardRef, useEffect } from 'react'
 import groq from 'groq'
 import type { InferGetStaticPropsType, GetStaticProps, NextPage } from 'next'
 import type { Page as SanityPage } from '@gen/sanity-schema'
@@ -7,6 +7,7 @@ import { getPageStaticProps } from '@lib/next'
 import { BODY_QUERY, filterDataToSingleItem } from '@studio/lib'
 import { BlockContent } from '@components/sanity'
 import PageTransition from '@components/transition/PageTransition'
+import { useRouter } from 'next/router'
 
 type PageRefType = React.ForwardedRef<HTMLDivElement>
 
@@ -37,7 +38,22 @@ const Page: NextPage<PageProps> = (
   { data, preview }: InferGetStaticPropsType<typeof getStaticProps>,
   ref: PageRefType
 ) => {
+  const router = useRouter()
   const page: SanityPage = filterDataToSingleItem(data)
+
+  useEffect(() => {
+    if (typeof window != 'undefined' && window.HubSpotConversations) {
+      const status = window.HubSpotConversations.widget.status()
+
+      if (router.asPath == '/' && !router.query.city && status.loaded) {
+        window.HubSpotConversations.widget.remove()
+      } else {
+        if (!status.loaded) {
+          window.HubSpotConversations.widget.load()
+        }
+      }
+    }
+  }, [router.asPath, router.query])
 
   return page?.body && (!page?._id.includes('drafts.') || preview) ? (
     <PageTransition ref={ref}>
