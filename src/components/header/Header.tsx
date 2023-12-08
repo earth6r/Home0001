49265abe5ad8659fccc89426c551fa7ce4963d23
James La Marre <jameslamarre@gmail.com>
2023-12-08 12:42:35 -0800

    fix: layout hotfix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -138,7 +138,7 @@ export const Header: FC<HeaderProps> = ({
 
           <AnimatedModal isOpen={inquiryOpen} onClose={onInquiryClose}>
             <div className="flex flex-col max-w-md md:max-w-none h-[calc(100%-var(--btn-height)-[6rem])] md:h-full py-6 md:py-10 pl-x md:pl-10">
-              <h2 className="text-xl font-bold uppercase pt-page">
+              <h2 className="text-xl font-bold uppercase pt-page md:pr-menu lg:pr-fullmenu">
                 {formSubmitted ? inquiry?.success || `Thanks!` : `Inquire`}
               </h2>
 
@@ -174,7 +174,7 @@ export const Header: FC<HeaderProps> = ({
             onClose={onBrokerInquiryClose}
           >
             <div className="flex flex-col max-w-md md:max-w-none h-[calc(100%-var(--btn-height)-[6rem])] md:h-full py-6 md:py-10 pl-x md:pl-10">
-              <h2 className="text-xl font-bold uppercase pt-page">
+              <h2 className="text-xl font-bold uppercase pt-page md:pr-menu lg:pr-fullmenu">
                 {formSubmitted
                   ? `Thanks for reaching out.  We will be in touch!`
                   : `Join our brokerage program`}
