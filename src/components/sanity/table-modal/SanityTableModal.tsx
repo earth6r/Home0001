5939f81c0a6bd3f576d7bc9ecd849a934e59efa6
James La Marre <jameslamarre@gmail.com>
2023-12-11 18:17:00 -0800

    fix: overflow on table omdal

--- src/components/sanity/table-modal/SanityTableModal.tsx
+++ src/components/sanity/table-modal/SanityTableModal.tsx
@@ -37,7 +37,7 @@ export const SanityTableModal: FC<SanityTableModalProps> = ({
   return (
     <div className={className}>
       <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        <div className="py-6 md:py-10 px-x md:px-10 h-full flex flex-col text-sm font-bold">
+        <div className="py-6 md:py-10 px-x md:px-10 h-full flex flex-col text-sm font-bold overflow-y-scroll">
           <div className="pb-y">
             {table.rows &&
               table.rows.length > 0 &&
