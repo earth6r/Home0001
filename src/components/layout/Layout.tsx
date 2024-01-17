cb2778bb7c33729a4aba96cfccd7d46a3e0b9874
James La Marre <jameslamarre@gmail.com>
2024-01-17 12:36:23 -0800

    test: mobile smooth scroll

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -67,7 +67,7 @@ export const Layout: FC<LayoutProps> = ({ children, data, siteSettings }) => {
           }}
           mainMenu={siteSettings?.mainMenu as Menus | undefined}
         />
-        <ReactLenis root>
+        <ReactLenis root options={{ smoothTouch: true }}>
           <main className="flex-auto">{children}</main>
         </ReactLenis>
         <Footer
