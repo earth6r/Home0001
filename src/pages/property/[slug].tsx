50d4d5d94e542957fdfbe527c57e9ee036acfb23
James La Marre <jameslamarre@gmail.com>
2024-01-18 13:03:16 -0800

    feat: added waitlist to property pages

--- src/pages/property/[slug].tsx
+++ src/pages/property/[slug].tsx
@@ -1,4 +1,4 @@
-import { ForwardRefRenderFunction, forwardRef } from 'react'
+import { ForwardRefRenderFunction, forwardRef, useState } from 'react'
 import groq from 'groq'
 import type {
   GetStaticPaths,
@@ -10,9 +10,11 @@ import type { Property as SanityPage } from '@gen/sanity-schema'
 import type { PageProps } from '@lib/next'
 import { getPageStaticProps } from '@lib/next'
 import { PROPERTIES_QUERY, client, filterDataToSingleItem } from '@studio/lib'
-import { BlockContent, RichText } from '@components/sanity'
 import { Property } from '@components/property'
 import PageTransition from '@components/transition/PageTransition'
+import { Waitlist } from '@components/waitlist'
+import { useForm } from 'react-hook-form'
+import classNames from 'classnames'
 
 type PageRefType = React.ForwardedRef<HTMLDivElement>
 
@@ -35,17 +37,48 @@ export const getStaticProps: GetStaticProps = context =>
   getPageStaticProps({ ...context, query: PROPERTY_QUERY })
 
 const PropertyPage: NextPage<PageProps> = (
-  { data, preview }: InferGetStaticPropsType<typeof getStaticProps>,
+  {
+    data,
+    siteSettings,
+    preview,
+  }: InferGetStaticPropsType<typeof getStaticProps>,
   ref: PageRefType
 ) => {
   const page: SanityPage = filterDataToSingleItem(data)
 
+  const { register, handleSubmit, reset, trigger, getValues } = useForm({
+    shouldUseNativeValidation: true,
+  })
+  const [formSubmitted, setFormSubmitted] = useState(false)
+  const [fullWidth, setFullWidth] = useState(false)
+
   return page?.header && (!page?._id.includes('drafts.') || preview) ? (
     <PageTransition ref={ref}>
       <article>
-        <div className="">
-          <Property property={page} className="w-full pt-page" />
-        </div>
+        <Property property={page} className="w-full pt-page" />
+
+        <Waitlist
+          waitlist={{
+            header: siteSettings.waitlistHeader,
+            text: siteSettings?.waitlistCopy,
+            id: siteSettings.wailistId,
+            successMessage: siteSettings?.waitlistSuccess,
+          }}
+          formActions={{
+            formSubmitted,
+            setFormSubmitted,
+            handleSubmit,
+            trigger,
+            register,
+            getValues,
+          }}
+          setFullWidth={() => setFullWidth(true)}
+          fullWidth={fullWidth}
+          className={classNames(
+            fullWidth ? 'md:left-0 md:w-full' : 'md:left-1/3 md:w-2/3',
+            'relative mt-page transition-all duration-200 ease-in-out'
+          )}
+        />
       </article>
     </PageTransition>
   ) : null
