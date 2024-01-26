b7fb553fd30e34e97ad4e48310ffe433472d506e
James La Marre <jameslamarre@gmail.com>
2024-01-26 08:47:12 -0800

    fix: design tweaks

--- src/components/waitlist/Waitlist.tsx
+++ src/components/waitlist/Waitlist.tsx
@@ -54,7 +54,7 @@ export const Waitlist: FC<WaitlistProps> = ({
 
   return (
     <div className={classNames(className)}>
-      <div className="h-[690px] md:h-[630px] pl-x pr-[calc(var(--space-menu)+var(--space-x))] pb-[41px] pt-[33px] md:px-x md:pb-[56px] md:pt-[38px] bg-yellow">
+      <div className="h-[630px] md:h-[630px] pl-x pr-[calc(var(--space-menu)+var(--space-x))] pb-[41px] pt-[33px] md:px-x md:pb-[56px] md:pt-[38px] bg-yellow">
         {formType === 'unit' && (
           <>
             <h2 className="text-lg font-bold uppercase pt-page md:pt-y md:pr-menu lg:pr-fullmenu">
