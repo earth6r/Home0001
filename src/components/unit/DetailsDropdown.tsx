a42b9bdc8c9bc2292366d58350b78c97243e9a5a
m00nface <hafeezdawood@gmail.com>
2024-01-30 20:10:43 +0000

    fix: padding details list

--- src/components/unit/DetailsDropdown.tsx
+++ src/components/unit/DetailsDropdown.tsx
@@ -25,7 +25,7 @@ const DetailsDropdown: FC<DetailsDropdownProps> = ({
     <>
       <RichText
         blocks={details.slice(0, 5)}
-        className="max-w-[500px] pr-menu md:pr-0"
+        className="max-w-[500px] md:pr-0"
       />
 
       <Disclosure defaultOpen={dropdownOpen}>
@@ -62,7 +62,7 @@ const DetailsDropdown: FC<DetailsDropdownProps> = ({
                   <div>
                     <RichText
                       blocks={details.slice(5, details.length)}
-                      className="max-w-[500px] pr-menu md:pr-0 mt-y md:mt-yhalf"
+                      className="max-w-[500px] md:pr-0 mt-y md:mt-yhalf"
                     />
                   </div>
                 </Disclosure.Panel>
