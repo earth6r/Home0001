384ed65eac39fee98971c0f6dbe5810bbe7cf727
James La Marre <jameslamarre@gmail.com>
2024-02-12 13:37:14 -0800

    feat: tracking form fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: ['cdn.sanity.io'],
     loader: 'custom',
