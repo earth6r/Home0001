6da726278385d0f6a9a3da2f91ff707583702072
James La Marre <jameslamarre@gmail.com>
2024-03-05 15:29:04 -0800

    feat: videos playing

--- src/components/sanity/blocks/videos/VideosBlock.tsx
+++ src/components/sanity/blocks/videos/VideosBlock.tsx
@@ -10,13 +10,16 @@ type VideosBlockProps = Omit<SanityBlockElement, keyof VideosBlockType> &
 export const VideosBlock: FC<VideosBlockProps> = ({ videos, className }) => {
   return (
     <Block
-      className={classNames(className, 'md:grid md:grid-cols-2 mt-y -mb-y')}
+      className={classNames(className, 'grid md:grid-cols-2 gap-x mt-y -mb-y')}
     >
       {videos &&
         videos.length > 0 &&
         videos.map(video => (
           <div key={video._key} className="md:col-span-1">
-            <SanityMedia video={video as SanityVideoType} />
+            <SanityMedia
+              video={video as SanityVideoType}
+              className="w-full aspect-square object-cover"
+            />
             {video.caption && (
               <p className="text-sm font-sansText font-medium text-right mt-y">
                 {video.caption}
