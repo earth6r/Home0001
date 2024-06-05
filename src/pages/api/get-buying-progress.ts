06155a01ab8a577373ce23ae9b099ed3fb7b3a8b
apinanyogaratnam <apinanapinan@icloud.com>
2024-06-05 17:45:20 -0400

    feat: progress

--- src/pages/api/get-buying-progress.ts
+++ src/pages/api/get-buying-progress.ts
@@ -1,33 +1,38 @@
-import { initializeAdmin } from '@lib/firebase/admin'
-import admin from 'firebase-admin'
+// Import necessary modules
+import { initializeAdmin } from '@lib/firebase/admin' // Function to initialize Firebase Admin SDK
+import admin from 'firebase-admin' // Firebase Admin SDK
 
-import { type NextApiRequest, type NextApiResponse } from 'next'
+import { type NextApiRequest, type NextApiResponse } from 'next' // Type definitions for Next.js API routes
 
 // Set configuration options for the API route
 export const config = {
-  maxDuration: 300, // 5 minutes max duration for the API route to respond to a request
+  maxDuration: 300, // Maximum duration for the API route to respond to a request (5 minutes)
 }
 
+// Handler function to process API requests
 // curl -X GET http://localhost:3000/api/get-buying-progress?email=apinanapinan@icloud.com
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const query = req.query
+  const query = req.query // Extract query parameters from the request
 
-  const email = query?.email
+  const email = query?.email // Extract the 'email' query parameter
 
+  // Check if email is provided
   if (!email) {
     res.status(400).json({
-      error: 'Missing email in request query',
+      error: 'Missing email in request query', // Respond with error if email is missing
     })
     return
   }
 
+  // Check if email is a string
   if (typeof email !== 'string') {
     res.status(400).json({
-      error: 'email must be a string',
+      error: 'email must be a string', // Respond with error if email is not a string
     })
     return
   }
 
+  // Mapping of buying progress stages to numerical values
   const mapBuyingProgress = {
     'escrow-deposit': 1,
     'download-documents': 2,
@@ -36,32 +41,35 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     completed: 5,
   }
 
-  initializeAdmin()
+  initializeAdmin() // Initialize Firebase Admin SDK
 
-  const db = admin.firestore()
+  const db = admin.firestore() // Get a reference to the Firestore database
 
+  // Query the 'users' collection for a document with the specified email
   const user = await db.collection('users').where('email', '==', email).get()
   if (user.empty) {
     res.status(400).json({
-      error: 'User not found',
+      error: 'User not found', // Respond with error if user is not found
     })
     return
   }
 
+  // Query the 'usersBuyingProgress' collection for the user's buying progress
   const buyingProgress = await db
     .collection('usersBuyingProgress')
     .where(admin.firestore.FieldPath.documentId(), '==', user.docs[0].id)
     .get()
   if (buyingProgress.empty) {
     res.status(200).json({
-      buyingProgress: null,
+      buyingProgress: null, // Respond with null if no buying progress is found
     })
     return
   }
 
+  // Update the user's buying progress
   await db.collection('usersBuyingProgress').doc(user.docs[0].id).update({
     // @ts-expect-error
-    buyingProgress: mapBuyingProgress[buyingProgress], // allowed items are 1 (escrow-deposit, default), 2 (download-documents), 3 (schedule-closing), 4 (full-payment), 5 (completed)
+    buyingProgress: mapBuyingProgress[buyingProgress], // Update the buying progress using the mapped value
   })
 }
 
