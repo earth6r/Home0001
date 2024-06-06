4df09ac31df5b0204a650d5113b54928e9179bc3
James La Marre <jameslamarre@gmail.com>
2024-06-06 13:38:03 -0700

    feat: signup

--- src/components/buy/BuyContainer.tsx
+++ src/components/buy/BuyContainer.tsx
@@ -14,9 +14,34 @@ export const BuyContainer: FC<BuyProps> = ({ className }) => {
   const [hasPassword, setHasPassword] = useState(false)
   const [loggedIn, setLoggedIn] = useState(false)
 
+  const [showDepositForm, setShowDepositForm] = useState(false)
+
+  const getBuyingProgress = async () => {
+    return await axios.post(
+      `/api/get-buying-progress?email=${router.query.email}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+  }
+
+  const accountSignIn = async () => {
+    return await axios.post(
+      `/api/login/signin`,
+      { email: router.query.email, password: router.query.password },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+  }
+
   const getAccount = async () => {
     return await axios.post(
-      `/api/login/check-email-existence`,
+      `/api/login/check-password-setup-for-email`,
       { email: router.query.email },
       {
         headers: {
@@ -27,9 +52,36 @@ export const BuyContainer: FC<BuyProps> = ({ className }) => {
   }
 
   useEffect(() => {
-    if (router.query.email) {
+    if (loggedIn) {
+      getBuyingProgress().then(res => {
+        console.log('getBuyingProgress res: ', res)
+        if (res.data.buyingProgress === null) {
+          // show deposit form
+          setShowDepositForm(true)
+        }
+      })
+    }
+  }, [loggedIn])
+
+  useEffect(() => {
+    if (router.query.password) {
+      // perform sign in if password is provided
+      accountSignIn().then(res => {
+        console.log('accountSignIn res: ', res)
+        if (res.data.user) {
+          setHasAccount(true)
+          setHasPassword(true)
+          setLoggedIn(true)
+        } else {
+          // if no password is set, show set password form
+          setHasAccount(true)
+          setHasPassword(false)
+        }
+      })
+    } else if (router.query.email) {
+      // check if account exists
       getAccount().then(res => {
-        console.log('useffect res: ', res)
+        console.log('getAccount res: ', res)
         if (res.data.user && res.data.user.email) {
           setHasAccount(true)
         } else {
@@ -37,19 +89,30 @@ export const BuyContainer: FC<BuyProps> = ({ className }) => {
           setHasAccount(false)
         }
       })
-    } else if (router.query.password) {
-      // perform signin instead
     }
   }, [router.query.email])
 
   return (
     <div className={classNames(className)}>
-      {hasAccount && !hasPassword ? (
-        <SetPasswordForm email={router.query.email as string} />
-      ) : (
-        hasAccount === false && (
-          <span className="text-button">{`Oops, looks like we can't find your account.`}</span>
-        )
+      {showDepositForm && (
+        <span className="text-button">{`Deposit form here`}</span>
+      )}
+
+      {/* {loggedIn && <span className="text-button">{`You're logged in.`}</span>} */}
+
+      {hasAccount && !hasPassword && (
+        <SetPasswordForm
+          email={router.query.email as string}
+          onPasswordSet={() => setTimeout(() => setHasPassword(true), 2000)}
+        />
+      )}
+
+      {hasAccount === false && (
+        <span className="text-button">{`Oops, looks like we can't find your account.`}</span>
+      )}
+
+      {!showDepositForm && !hasAccount && (
+        <span className="text-button">{`Loading...`}</span>
       )}
     </div>
   )
--- src/components/buy/SetPasswordForm.tsx
+++ src/components/buy/SetPasswordForm.tsx
@@ -1,24 +1,19 @@
 import type { FC } from 'react'
 import React, { HTMLAttributes, useEffect, useState } from 'react'
 import classNames from 'classnames'
-import {
-  FieldValues,
-  UseFormHandleSubmit,
-  useForm,
-  useWatch,
-} from 'react-hook-form'
-import { submitForm } from '@lib/util/submit-forms'
+import { useForm } from 'react-hook-form'
 import axios from 'axios'
-import IconRightArrowBold from '@components/icons/IconRightArrowBold'
 import IconSmallArrow from '@components/icons/IconSmallArrow'
 import Link from 'next/link'
 
 interface SetPasswordFormProps extends HTMLAttributes<HTMLFormElement> {
   email?: string
+  onPasswordSet?: () => void
 }
 
 export const SetPasswordForm: FC<SetPasswordFormProps> = ({
   email,
+  onPasswordSet,
   className,
 }) => {
   const {
@@ -32,12 +27,15 @@ export const SetPasswordForm: FC<SetPasswordFormProps> = ({
   const [formSubmitted, setFormSubmitted] = useState(false)
   const [formError, setFormError] = useState<unknown | string | null>(null)
 
+  useEffect(() => {
+    if (formSubmitted) onPasswordSet
+  }, [formSubmitted])
+
   const onSubmit = async (data: any) => {
-    let result
     try {
-      result = await axios.post(
+      await axios.post(
         `/api/login/set-password`,
-        { email: email, password: data.password },
+        { email: data.email, password: data.password },
         {
           headers: {
             'Content-Type': 'application/json',
@@ -56,86 +54,80 @@ export const SetPasswordForm: FC<SetPasswordFormProps> = ({
 
   return (
     <div className={classNames(className)}>
-      {formSubmitted ? (
-        <div className="relative mt-ydouble mb-2">
-          <p>{`Form submitted`}</p>
-        </div>
-      ) : (
-        <form onSubmit={handleSubmit(onSubmit)} className="w-full h-full">
-          <div
-            className={classNames(
-              className,
-              'w-full md:max-w-[526px] rich-text'
-            )}
-          >
-            <h2>{`Finish creating an account`}</h2>
-            <p>{`0001 homes are released exclusively to our waitlist. Sign up here to continue:`}</p>
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full h-full">
+        <div
+          className={classNames(className, 'w-full md:max-w-[526px] rich-text')}
+        >
+          <h2>{`Finish creating an account`}</h2>
+          <p>{`0001 homes are released exclusively to our waitlist. Sign up here to continue:`}</p>
 
-            <div className={classNames('relative flex flex-col gap-y')}>
-              <div className="relative flex flex-col gap-y">
-                <input
-                  placeholder={'YOUR EMAIL'}
-                  type="email"
-                  id="email"
-                  className="hidden"
-                  value={email}
-                  required
-                  {...register('email')}
-                />
+          <div className={classNames('relative flex flex-col gap-y')}>
+            <div className="relative flex flex-col gap-y">
+              <input
+                placeholder={'YOUR EMAIL'}
+                type="email"
+                id="email"
+                className="hidden"
+                value={email}
+                required
+                {...register('email')}
+              />
 
-                <input
-                  placeholder={'PASSWORD'}
-                  type="password"
-                  id="password"
-                  className="input disabled:opacity-50"
-                  required
-                  {...register('password')}
-                />
+              <input
+                placeholder={'PASSWORD'}
+                type="password"
+                id="password"
+                className="input disabled:opacity-50"
+                required
+                {...register('password')}
+              />
 
-                <input
-                  placeholder={'RE-ENTER PASSWORD'}
-                  type="password"
-                  id="confirm_password"
-                  className="input disabled:opacity-50"
-                  required
-                  {...register('confirm_password', {
-                    validate: (val: string) => {
-                      if (watch('password') != val) {
-                        return 'Your passwords do no match'
-                      }
-                    },
-                  })}
-                />
-              </div>
+              <input
+                placeholder={'RE-ENTER PASSWORD'}
+                type="password"
+                id="confirm_password"
+                className="input disabled:opacity-50"
+                required
+                {...register('confirm_password', {
+                  validate: (val: string) => {
+                    if (watch('password') != val) {
+                      return 'Your passwords do no match'
+                    }
+                  },
+                })}
+              />
+            </div>
 
-              <p className="my-y">
-                {`By creating an account you agree to HOME0001’s `}
-                <Link href="./legal">{`Terms & Conditions`}</Link>
-              </p>
+            <p className="my-y">
+              {`By creating an account you agree to HOME0001’s `}
+              <Link href="./legal">{`Terms & Conditions`}</Link>
+            </p>
 
-              <div
-                className={classNames('relative flex flex-col gap-2 md:gap-y')}
+            <div
+              className={classNames('relative flex flex-col gap-2 md:gap-y')}
+            >
+              <button
+                className="relative flex justify-between items-center w-full md:w-btnWidth px-x h-btn text-center uppercase text-white bg-black font-medium text-xs z-above"
+                type={'submit'}
+                disabled={isSubmitting}
               >
-                <button
-                  className="relative flex justify-between items-center w-full md:w-btnWidth px-x h-btn text-center uppercase text-white bg-black font-medium text-xs z-above"
-                  type={'submit'}
-                  disabled={isSubmitting}
-                >
-                  {isSubmitting ? 'Submitting...' : 'Create account'}
-                  <IconSmallArrow
-                    className="w-[15px] md:w-[17px]"
-                    height="10"
-                  />
-                </button>
-              </div>
+                {isSubmitting ? 'Submitting...' : 'Create account'}
+                <IconSmallArrow className="w-[15px] md:w-[17px]" height="10" />
+              </button>
             </div>
           </div>
+        </div>
 
-          {formError !== null && (
-            <p className="text-red mt-y font-medium uppercase">{`Error setting password`}</p>
-          )}
-        </form>
-      )}
+        {formError !== null && (
+          <p className="text-red mt-y font-medium uppercase">{`Error setting password`}</p>
+        )}
+
+        {formSubmitted && (
+          <div className="relative mt-ydouble mb-2">
+            <p className="font-medium uppercase">{`Form submitted, please wait...`}</p>
+          </div>
+        )}
+      </form>
     </div>
   )
 }
