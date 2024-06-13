9f7a52162a1210afa23ccc2e986f35fa115400ab
James La Marre <jameslamarre@gmail.com>
2024-06-13 16:21:03 -0700

    test: function level cors headers

--- src/components/buy/BuyCalendar.tsx
+++ src/components/buy/BuyCalendar.tsx
@@ -34,6 +34,11 @@ export const BuyCalendar: FC<BuyCalendarProps> = ({ email, className }) => {
     return await axios.post(`${BASE_URL}/api/google/available-meeting-hours`, {
       headers: {
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+        'Access-Control-Allow-Headers':
+          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
       },
     })
   }
@@ -70,6 +75,11 @@ export const BuyCalendar: FC<BuyCalendarProps> = ({ email, className }) => {
         {
           headers: {
             'Content-Type': 'application/json',
+            'Access-Control-Allow-Credentials': 'true',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+            'Access-Control-Allow-Headers':
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
           },
         }
       )
