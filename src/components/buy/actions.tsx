6c16655fe0cce4b73ecade4d79f768583e5e968f
James La Marre <jameslamarre@gmail.com>
2024-06-14 15:22:05 -0700

    feat: additional emails input

--- src/components/buy/actions.tsx
+++ src/components/buy/actions.tsx
@@ -56,7 +56,8 @@ export const getAvailableSlots = async () => {
 export const createGoogleCalendarMeeting = async (
   data: any,
   email: string,
-  unit: string
+  unit: string,
+  additionalEmails: string[]
 ) => {
   return await axios.post(
     `${BASE_URL}/api/google/create-google-calendar_meeting`,
@@ -77,6 +78,7 @@ export const createGoogleCalendarMeeting = async (
       ],
       location: 'Link will be emailed',
       eventDescription: `Home0001 closing for ${unit}`,
+      additionalEmails: additionalEmails,
     },
     {
       headers: {
