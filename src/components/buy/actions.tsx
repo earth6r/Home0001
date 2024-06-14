a2d93c2f282e03c074815e739aa41b4a6de2806b
James La Marre <jameslamarre@gmail.com>
2024-06-14 13:57:18 -0700

    feat: better password checking

--- src/components/buy/actions.tsx
+++ src/components/buy/actions.tsx
@@ -7,6 +7,7 @@ const CONFIG = {
   },
 }
 
+// db endpoints
 export const getBuyingProgress = async (email: string) => {
   return await axios.get(
     `${BASE_URL}/api/get-buying-progress?email=${email}`,
@@ -27,5 +28,60 @@ export const getAccount = async (email: string) => {
     `${BASE_URL}/api/login/check-password-setup-for-email`,
     { email: email },
     CONFIG
+  )
+}
+
+// stripe
+export const setPaymentIntent = async (unit: string) => {
+  return await axios.post(
+    `${BASE_URL}/api/create-stripe-payment`,
+    { propertyType: unit },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+}
+
+// google calendar api
+export const getAvailableSlots = async () => {
+  return await axios.post(`${BASE_URL}/api/google/available-meeting-hours`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
+
+export const createGoogleCalendarMeeting = async (
+  data: any,
+  email: string,
+  unit: string
+) => {
+  return await axios.post(
+    `${BASE_URL}/api/google/create-google-calendar_meeting`,
+    {
+      date: data.date,
+      startTime: data.startTime,
+      eventName: 'HOME0001 Closing',
+      inviteeEmail: email,
+      staffEmails: [
+        'dzelefsky@braverlaw.net',
+        'scott@choicefamily.com',
+        'Matthew@omnititle.com',
+        'gio@choicefamily.com',
+        'andres@hoggholdings.com',
+        'annika@home0001.com',
+        'yan@home0001.com',
+        'm@choicefamily.com',
+      ],
+      location: 'Link will be emailed',
+      eventDescription: `Home0001 closing for ${unit}`,
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
   )
 }
