ee624a8fcbe4748f7f9f1459ecb9cd7f89bdfd68
James La Marre <jameslamarre@gmail.com>
2024-06-20 12:10:59 -0700

    feat: calendar fixes

--- src/components/buy/BuyCalendar.tsx
+++ src/components/buy/BuyCalendar.tsx
@@ -15,12 +15,14 @@ import IconChevron from '@components/icons/IconChevron'
 interface BuyCalendarProps extends HTMLAttributes<HTMLFormElement> {
   email?: string
   unit?: string
+  calendarDate?: string
   onMeetingSet: () => void
 }
 
 export const BuyCalendar: FC<BuyCalendarProps> = ({
   email,
   unit,
+  calendarDate,
   onMeetingSet,
   className,
 }) => {
@@ -76,9 +78,7 @@ export const BuyCalendar: FC<BuyCalendarProps> = ({
   }, [])
 
   return (
-    <div
-      className={classNames(className, 'px-x py-ydouble bg-yellow rich-text')}
-    >
+    <div className={classNames(className, 'px-x py-ydouble bg-yellow')}>
       <div className="rich-text">
         <p className="mb-y text-button font-sansText">{`When do you want to close?`}</p>
         <p>Select a time for your document review with a notary.</p>
@@ -99,135 +99,147 @@ export const BuyCalendar: FC<BuyCalendarProps> = ({
         </p>
       </div>
 
-      {!formSubmitted && (
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="w-full md:max-w-[526px] h-full mt-ydouble"
-        >
-          <div className="flex flex-col justify-start gap-y mb-ydouble">
-            {loading && <p className="!mx-0 mt-y">{`Loading...`}</p>}
+      {calendarDate && (
+        <div className="mt-ydouble">
+          <span className="inline-block mb-y text-md uppercase font-sansText">{`Your buying session has been requested for:`}</span>
+          <h2 className="text-h2">{`${calendarDate}`}</h2>
+        </div>
+      )}
 
-            <Swiper
-              ref={slidesRef}
-              modules={[Navigation]}
-              loop={false}
-              spaceBetween={16}
-              speed={600}
-              slidesPerView={1}
-              navigation={{
-                nextEl: '.swiper-next',
-                prevEl: '.swiper-prev',
-              }}
-              className={classNames(
-                'calendar w-full ml-0 md:mx-auto overflow-hidden cursor-grab'
-              )}
-            >
-              {availableSlots.map(
-                ({ date, slots }: { date: string; slots: string[] }, index) => {
-                  const formattedDate = new Date(date).toLocaleDateString(
-                    'en-US',
-                    {
-                      weekday: undefined,
-                      year: 'numeric',
-                      month: 'long',
-                      day: 'numeric',
-                    }
-                  )
+      {!formSubmitted ||
+        (!calendarDate && (
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="w-full md:max-w-[526px] h-full mt-ydouble"
+          >
+            <div className="flex flex-col justify-start gap-y mb-ydouble">
+              {loading && <p className="!mx-0 mt-y">{`Loading...`}</p>}
 
-                  return (
-                    <SwiperSlide key={index} className="block h-full">
-                      <div className="mb-y">
-                        <input
-                          type="radio"
-                          id={date}
-                          value={date}
-                          className="!hidden"
-                          {...register('date')}
-                        />
-                        <label htmlFor={date} className="text-button">
-                          {formattedDate}
-                        </label>
-                      </div>
-
-                      <div className="grid grid-cols-3 gap-xhalf">
-                        {slots?.map((time: string, index: number) => (
-                          <div
-                            key={index}
-                            className="flex items-center justify-center relative w-full h-btn cursor-pointer"
-                          >
-                            <input
-                              type="radio"
-                              id={time}
-                              value={time}
-                              className="background-checkbox"
-                              {...register('startTime')}
-                              onClick={() => {
-                                const dateEl = document.getElementById(date)
-                                if (dateEl) {
-                                  dateEl.click()
-                                }
-                              }}
-                            />
-                            <label
-                              htmlFor={time}
-                              className="relative cursor-pointer z-above"
-                            >
-                              {time}
-                            </label>
-                          </div>
-                        ))}
-                      </div>
-                    </SwiperSlide>
-                  )
-                }
-              )}
-              <div
+              <Swiper
+                ref={slidesRef}
+                modules={[Navigation]}
+                loop={false}
+                spaceBetween={16}
+                speed={600}
+                slidesPerView={1}
+                navigation={{
+                  nextEl: '.swiper-next',
+                  prevEl: '.swiper-prev',
+                }}
                 className={classNames(
-                  'flex absolute top-0 right-0 transition-opacity duration-200 pointer-events-none z-above'
+                  'calendar w-full ml-0 md:mx-auto overflow-hidden cursor-grab'
                 )}
               >
-                <div className="p-x transform -translate-y-1/3 swiper-prev pointer-events-auto cursor-pointer">
-                  <IconChevron
-                    width="13"
-                    fill="black"
-                    className={classNames('rotate-180')}
-                  />
+                {availableSlots.map(
+                  (
+                    { date, slots }: { date: string; slots: string[] },
+                    index
+                  ) => {
+                    const formattedDate = new Date(date).toLocaleDateString(
+                      'en-US',
+                      {
+                        weekday: undefined,
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric',
+                      }
+                    )
+
+                    return (
+                      <SwiperSlide key={index} className="block h-full">
+                        <div className="mb-y">
+                          <input
+                            type="radio"
+                            id={date}
+                            value={date}
+                            className="!hidden"
+                            {...register('date')}
+                          />
+                          <label htmlFor={date} className="text-button">
+                            {formattedDate}
+                          </label>
+                        </div>
+
+                        <div className="grid grid-cols-3 gap-xhalf">
+                          {slots?.map((time: string, index: number) => (
+                            <div
+                              key={index}
+                              className="flex items-center justify-center relative w-full h-btn cursor-pointer"
+                            >
+                              <input
+                                type="radio"
+                                id={time}
+                                value={time}
+                                className="background-checkbox"
+                                {...register('startTime')}
+                                onClick={() => {
+                                  const dateEl = document.getElementById(date)
+                                  if (dateEl) {
+                                    dateEl.click()
+                                  }
+                                }}
+                              />
+                              <label
+                                htmlFor={time}
+                                className="relative cursor-pointer z-above"
+                              >
+                                {time}
+                              </label>
+                            </div>
+                          ))}
+                        </div>
+                      </SwiperSlide>
+                    )
+                  }
+                )}
+                <div
+                  className={classNames(
+                    'flex absolute top-0 right-0 transition-opacity duration-200 pointer-events-none z-above'
+                  )}
+                >
+                  <div className="p-x transform -translate-y-1/3 swiper-prev pointer-events-auto cursor-pointer">
+                    <IconChevron
+                      width="13"
+                      fill="black"
+                      className={classNames('rotate-180')}
+                    />
+                  </div>
+                  <div className="p-x transform -translate-y-1/3 swiper-next pointer-events-auto cursor-pointer">
+                    <IconChevron
+                      width="13"
+                      fill="black"
+                      className={classNames('relative')}
+                    />
+                  </div>
                 </div>
-                <div className="p-x transform -translate-y-1/3 swiper-next pointer-events-auto cursor-pointer">
-                  <IconChevron
-                    width="13"
-                    fill="black"
-                    className={classNames('relative')}
-                  />
-                </div>
-              </div>
-            </Swiper>
+              </Swiper>
 
-            <input
-              type="email"
-              className="input"
-              placeholder="Additional emails (comma separated)"
-              multiple
-              {...register('additionalEmails')}
-            />
-          </div>
+              <input
+                type="email"
+                className="input"
+                placeholder="Additional emails (comma separated)"
+                multiple
+                {...register('additionalEmails')}
+              />
+            </div>
 
-          <button
-            className="relative flex justify-between items-center w-full md:w-btnWidth px-x h-btn text-center uppercase text-white bg-black font-medium text-xs z-above"
-            type={'submit'}
-            disabled={isSubmitting}
-          >
-            {isSubmitting ? 'Submitting...' : 'Confirm'}
-            <IconSmallArrow className="w-[15px] md:w-[17px]" height="10" />
-          </button>
+            <button
+              className="relative flex justify-between items-center w-full md:w-btnWidth px-x h-btn text-center uppercase text-white bg-black font-medium text-xs z-above"
+              type={'submit'}
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? 'Submitting...' : 'Confirm'}
+              <IconSmallArrow className="w-[15px] md:w-[17px]" height="10" />
+            </button>
 
-          {formError.error && (
-            <p className="text-red mt-y font-medium uppercase">
-              {formError.message || `Meeting creation failed, please try again`}
-            </p>
-          )}
-        </form>
-      )}
+            {formError.error && (
+              <p className="text-red mt-y font-medium uppercase">
+                {formError.message ||
+                  `Meeting creation failed, please try again`}
+              </p>
+            )}
+          </form>
+        ))}
 
       {formSubmitted && (
         <div className="relative mt-ydouble mb-2">
