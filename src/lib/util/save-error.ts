df4990f93c0aca5bd83e4480d5f4ac885943d007
apinanyogaratnam <apinanapinan@icloud.com>
2024-06-30 15:05:12 -0400

    feat: progress

--- src/lib/util/save-error.ts
+++ src/lib/util/save-error.ts
@@ -1,12 +1,14 @@
 import axios from 'axios'
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+
 export const saveError = (
   error: any,
   errorType: string,
   statusCode: number | null = null
 ) => {
   // TODO: save userAgent and user IP address
-  return axios.post('/api/errors/save-to-firestore', {
+  return axios.post(`${BASE_URL}/api/errors/save-to-firestore`, {
     error: JSON.stringify(error),
     errorType,
     statusCode,
