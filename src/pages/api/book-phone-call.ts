4ff923f68773c5f865c73d689cd510cc899a6ec5
apinanyogaratnam <apinanapinan@icloud.com>
2024-07-05 17:03:15 -0400

    feat: progress

--- src/pages/api/book-phone-call.ts
+++ src/pages/api/book-phone-call.ts
@@ -7,7 +7,7 @@ export const config = {
   maxDuration: 300, // Maximum duration for the API route to respond to a request (5 minutes)
 }
 
-// curl -X POST http://localhost:3000/api/book-phone-call -H "Content-Type: application/json" -d '{"email":"apinanapinan@icloud.com","timestamp":"2022-01-01T00:00:00Z","phoneNumber":"1234567890"}'
+// curl -X POST http://localhost:3000/api/book-phone-call -H "Content-Type: application/json" -d '{"email":"apinanapinan@icloud.com","timestamp":"1720441800000","phoneNumber":"1234567890"}'
 // TODO: might need to add phone number field
 export default async function handler(
   req: NextApiRequest,
@@ -48,6 +48,14 @@ export default async function handler(
   if (typeof timestamp !== 'string') {
     res.status(400).json({
       error: 'timestamp must be a string', // Respond with error if timestamp is not a string
+    })
+    return
+  }
+
+  // if timestamp converted to number is invalid, respond with error
+  if (isNaN(Number(timestamp))) {
+    res.status(400).json({
+      error: 'Invalid timestamp', // Respond with error if timestamp is invalid
     })
     return
   }
