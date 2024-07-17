77cb12703f65625becb7b3ea8721304a3b1098c1
James La Marre <jameslamarre@gmail.com>
2024-07-17 15:27:20 -0700

    feat: moment timezone

--- src/components/sanity/blocks/calendar/actions.tsx
+++ src/components/sanity/blocks/calendar/actions.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import moment from 'moment-timezone'
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 const CONFIG = {
@@ -16,11 +17,15 @@ export const getAvailableSlots = async () => {
 }
 
 export const bookPhoneCall = async (data: any) => {
-  const startDateTime = new Date(`${data.date} ${data.startTime}`)
-  const startDateTimePlus = new Date(`${data.date} ${data.startTime}`)
-  const endDateTime = new Date(
-    startDateTimePlus.setMinutes(startDateTimePlus.getMinutes() + 15)
+  const startDateTime = moment.tz(
+    `${data.date} ${data.startTime}`,
+    'America/New_York'
   )
+  const startDateTimePlus = moment.tz(
+    `${data.date} ${data.startTime}`,
+    'America/New_York'
+  )
+  const endDateTime = startDateTimePlus.add(15, 'minutes')
   return await axios.post(
     `${BASE_URL}/api/bookings/book-phone-call`,
     {
