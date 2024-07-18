c3df6cba6a3c3e69c34cfcaa8f2e34b255414bea
apinanyogaratnam <apinanapinan@icloud.com>
2024-07-18 11:04:54 -0400

    fix: fixes

--- src/pages/api/bookings/send-whatsapp-booked-message.ts
+++ src/pages/api/bookings/send-whatsapp-booked-message.ts
@@ -8,12 +8,12 @@ export const sendWhatsappBookedMessage = async (
   const message = `A HOME0001 phone call has been booked by ${firstName} ${lastName} on ${startTimestamp}.`
 
   const numbers = [
-    '+15038676436',
-    '+4915168698913',
-    '+17134103755',
-    '+19175824100',
-    '+447577459373',
-    '+491634841797',
+    // '+15038676436',
+    // '+4915168698913',
+    '+17134103755', // yan
+    // '+19175824100',
+    // '+447577459373',
+    // '+491634841797',
   ]
 
   for (const number of numbers) {
