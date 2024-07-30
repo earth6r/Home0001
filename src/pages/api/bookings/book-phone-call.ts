cf71c8e7e9a7dffc777c76c0bf8f22fd3a06f759
YannyD <yan@earth.net>
2024-07-30 13:44:59 -0500

    fix: event copy

--- src/pages/api/bookings/book-phone-call.ts
+++ src/pages/api/bookings/book-phone-call.ts
@@ -66,7 +66,7 @@ export default async function handler(
       endTime: endTimestamp,
       eventName: 'Zoom with HOME0001',
       inviteeEmail: email,
-      eventDescription: `You're scheduled for a zoom call with HOME0001.`,
+      eventDescription: `You're scheduled for a Zoom call with HOME0001.`,
     })
   } catch (error) {
     // eslint-disable-next-line no-console
