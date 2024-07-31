0ae64d8c1d7deb24424a05c3148906445c9b86a3
Scott Wynn <scott@hubmation.com>
2024-07-31 11:27:46 +0530

    check timezone

--- src/pages/api/google/available-meeting-hours.ts
+++ src/pages/api/google/available-meeting-hours.ts
@@ -141,7 +141,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     })
 
     const allAvailableSlots = await Promise.all(datePromises)
-    res.status(200).json({ data: allAvailableSlots })
+    res.status(200).json({ data: allAvailableSlots,currentdate:new Date().toString() })
   } catch (error) {
     console.error(error)
     res.status(500).json({ error: 'Internal Server Error' })
