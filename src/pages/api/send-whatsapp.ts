5fa3d5601a6368277658a458eb22a01bdae0d772
apinanyogaratnam <apinanapinan@icloud.com>
2024-08-12 19:41:59 -0400

    feat: property tour reminder endpoint

--- src/pages/api/send-whatsapp.ts
+++ src/pages/api/send-whatsapp.ts
@@ -62,6 +62,84 @@ export const sendMessage = async (recipientPhone: string, message: string) => {
   }
 }
 
+export const sendPropertyTourBookedIn1HourMessage = async (
+  recipientPhone: string,
+  name: string,
+  email: string,
+  date: string,
+  phoneNumber: string,
+  details: string
+) => {
+  const authToken = process.env.WHATSAPP_PERMANENT_TOKEN
+
+  const data = {
+    messaging_product: 'whatsapp',
+    to: recipientPhone,
+    type: 'template',
+    template: {
+      name: 'property_tour_event_in_1h',
+      language: {
+        code: 'en',
+      },
+      components: [
+        {
+          type: 'body',
+          parameters: [
+            {
+              type: 'text',
+              text: name,
+            },
+            {
+              type: 'text',
+              text: email,
+            },
+            {
+              type: 'text',
+              text: date,
+            },
+            {
+              type: 'text',
+              text: phoneNumber,
+            },
+            {
+              type: 'text',
+              text: details,
+            },
+          ],
+        },
+      ],
+    },
+  }
+
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    }
+
+    const response = await axios.post(
+      'https://graph.facebook.com/v13.0/307932205726236/messages',
+      data,
+      config
+    )
+
+    // TODO: discuss with @YannyD and fix this
+    // await axios.post(
+    //   `https://us-central1-homeearthnet.cloudfunctions.net/initialMessage`,
+    //   {
+    //     to: recipientPhone,
+    //     message: message,
+    //   }
+    // )
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error("Couldn't send message", error)
+    saveError(error, 'sendMessage')
+    throw error
+  }
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
