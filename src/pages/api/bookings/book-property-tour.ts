b150a98fa4cb4d06b44c5123b5605bd6edfde377
YannyD <yan@earth.net>
2024-08-26 18:34:04 -0500

    fix: event header for tours

--- src/pages/api/bookings/book-property-tour.ts
+++ src/pages/api/bookings/book-property-tour.ts
@@ -88,7 +88,7 @@ export default async function handler(
     const googleCalendarEventId = await createCalendarEvent({
       startTime: startTimestamp,
       endTime: endTimestamp,
-      eventName: 'Property Tour with HOME0001',
+      eventName: 'Tour HOME0001 Lower East Side',
       inviteeEmail: email,
       eventDescription: `You'll be meeting a member of the collective in front of the HOME0001 building at 48 Allen, between Hester and Grand Street.<br><br>Feel free to call or message us anytime at +1 973-791-5529 (SMS & WhatsApp) in case you're running late, need to reschedule or have any questions.`,
       calendarEmail: 'lowereastside@home0001.com',
