8abcf80a5a551cb418b0f17775aff6bc521cfc10
apinanyogaratnam <apinanapinan@icloud.com>
2024-09-18 15:52:01 -0400

    feat: add note endpoint

---
+++ src/pages/api/hubspot/add-note.ts
@@ -0,0 +1,83 @@
+import { NextApiRequest, NextApiResponse } from 'next/types'
+import { updateHubspotContactProperties } from '@lib/util/update-hubspot-contact'
+import axios from 'axios'
+import { saveError } from '@lib/util/save-error'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  const { email } = req.query as { email: string }
+  const { note } = req.body
+
+  const response = await axios.get(
+    `https://api.hubapi.com/crm/v3/objects/contacts`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_HUBSPOT_API_KEY}`,
+      },
+    }
+  )
+
+  const contact = response.data.results.find(
+    (contact: any) => contact.properties.email === email
+  )
+
+  if (!contact) {
+    res.status(404).json({
+      status: 'error',
+      message: 'Contact not found',
+    })
+
+    return
+  }
+
+  const hubspotContactId = contact.id
+
+  try {
+    await axios.patch(
+      `https://api.hubapi.com/engagements/v1/engagements`,
+      {
+        engagement: {
+          active: true,
+          type: 'NOTE',
+          timestamp: new Date().getTime(),
+        },
+        associations: {
+          contactIds: [hubspotContactId],
+          companyIds: [],
+          dealIds: [],
+          ownerIds: [],
+        },
+        metadata: {
+          body: ``,
+        },
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_HUBSPOT_API_KEY}`,
+        },
+      }
+    )
+  } catch (error: any) {
+    console.error('Error updating Hubspot contact properties', error)
+    saveError(
+      {
+        error,
+        additionalInfo: {
+          email,
+          note,
+        },
+      },
+      'updateHubspotContactProperties'
+    )
+
+    throw error
+  }
+
+  res.status(200).json({
+    status: 'success',
+  })
+}
