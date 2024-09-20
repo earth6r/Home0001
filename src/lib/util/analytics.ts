70517187203d7404a507a11a356e3355b24ceee9
James La Marre <jameslamarre@gmail.com>
2024-09-20 10:43:44 -0700

    fix: removed site settings log

--- src/lib/util/analytics.ts
+++ src/lib/util/analytics.ts
@@ -4,9 +4,6 @@ const GOOGLE_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_ID
 export const sendGoogleEvent = (event: string, options?: {}) => {
   if (typeof window !== 'undefined' && GOOGLE_ID) {
     window.dataLayer = window.dataLayer || []
-    const isProductionSite =
-      window?.location?.origin === 'https://www.home0001.com'
-
     window.dataLayer.push({
       event: 'beta_' + event,
       options,
