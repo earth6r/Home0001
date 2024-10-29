0de4acb9e0da1162135779b0345099a1eba9b3ca
apinanyogaratnam <apinanapinan@icloud.com>
2024-10-29 10:00:57 -0400

    feat: calling rocketchat livechat for /chat page for whatsapp

---
+++ src/pages/api/send-whatsapp-livechat.ts
@@ -0,0 +1,342 @@
+import { saveError } from '@lib/util/save-error'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { createVisitorAndChatRoom } from './rocketchat/livechat'
+
+const axios = require('axios')
+
+type Data = {
+  message: string
+  error?: unknown
+}
+
+export const config = {
+  maxDuration: 300,
+}
+
+export const sendMessage = async (
+  recipientPhone: string,
+  message: string,
+  template: string | null = null,
+  initialMessage: boolean = true
+) => {
+  const authToken = process.env.WHATSAPP_PERMANENT_TOKEN
+
+  const data = {
+    messaging_product: 'whatsapp',
+    to: recipientPhone,
+    type: 'template',
+    template: {
+      name: template || 'primary_test',
+      language: {
+        code: 'en',
+      },
+      components: [
+        {
+          type: 'body',
+          parameters: [
+            {
+              type: 'text',
+              text: message,
+            },
+          ],
+        },
+      ],
+    },
+  }
+
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    }
+
+    const response = await axios.post(
+      'https://graph.facebook.com/v13.0/307932205726236/messages',
+      data,
+      config
+    )
+
+    if (initialMessage) {
+      await axios.post(
+        `https://us-central1-homeearthnet.cloudfunctions.net/initialMessage`,
+        {
+          to: recipientPhone,
+          message: message,
+        }
+      )
+    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error("Couldn't send message", error)
+    saveError(error, 'sendMessage')
+    throw error
+  }
+}
+
+export const sendWAMessagePropertyTourBooked = async (
+  recipientPhone: string,
+  message: string
+) => {
+  const authToken = process.env.WHATSAPP_PERMANENT_TOKEN
+
+  const data = {
+    messaging_product: 'whatsapp',
+    to: recipientPhone,
+    type: 'template',
+    template: {
+      name: 'property_tour_booked',
+      language: {
+        code: 'en',
+      },
+      components: [
+        {
+          type: 'body',
+          parameters: [
+            {
+              type: 'text',
+              text: message,
+            },
+          ],
+        },
+      ],
+    },
+  }
+
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    }
+
+    const response = await axios.post(
+      'https://graph.facebook.com/v13.0/307932205726236/messages',
+      data,
+      config
+    )
+
+    await axios.post(
+      `https://us-central1-homeearthnet.cloudfunctions.net/initialMessage`,
+      {
+        to: recipientPhone,
+        message: message,
+      }
+    )
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error("Couldn't send message", error)
+    saveError(error, 'sendMessage')
+    throw error
+  }
+}
+
+export const sendWAMessageReschedulePhoneCall = async (
+  recipientPhone: string,
+  message: string
+) => {
+  const authToken = process.env.WHATSAPP_PERMANENT_TOKEN
+
+  const data = {
+    messaging_product: 'whatsapp',
+    to: recipientPhone,
+    type: 'template',
+    template: {
+      name: 'phone_call_rescheduled',
+      language: {
+        code: 'en',
+      },
+      components: [
+        {
+          type: 'body',
+          parameters: [
+            {
+              type: 'text',
+              text: message,
+            },
+          ],
+        },
+      ],
+    },
+  }
+
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    }
+
+    const response = await axios.post(
+      'https://graph.facebook.com/v13.0/307932205726236/messages',
+      data,
+      config
+    )
+
+    await axios.post(
+      `https://us-central1-homeearthnet.cloudfunctions.net/initialMessage`,
+      {
+        to: recipientPhone,
+        message: message,
+      }
+    )
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error("Couldn't send message", error)
+    saveError(error, 'sendMessage')
+    throw error
+  }
+}
+
+export const sendWAMessageReschedulePropertyTour = async (
+  recipientPhone: string,
+  message: string
+) => {
+  const authToken = process.env.WHATSAPP_PERMANENT_TOKEN
+
+  const data = {
+    messaging_product: 'whatsapp',
+    to: recipientPhone,
+    type: 'template',
+    template: {
+      name: 'property_tour_rescheduled',
+      language: {
+        code: 'en',
+      },
+      components: [
+        {
+          type: 'body',
+          parameters: [
+            {
+              type: 'text',
+              text: message,
+            },
+          ],
+        },
+      ],
+    },
+  }
+
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    }
+
+    const response = await axios.post(
+      'https://graph.facebook.com/v13.0/307932205726236/messages',
+      data,
+      config
+    )
+
+    await axios.post(
+      `https://us-central1-homeearthnet.cloudfunctions.net/initialMessage`,
+      {
+        to: recipientPhone,
+        message: message,
+      }
+    )
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error("Couldn't send message", error)
+    saveError(error, 'sendMessage')
+    throw error
+  }
+}
+
+export const sendPropertyTourBookedIn1HourMessage = async (
+  recipientPhone: string,
+  name: string,
+  email: string,
+  date: string,
+  phoneNumber: string,
+  details: string
+) => {
+  const authToken = process.env.WHATSAPP_PERMANENT_TOKEN
+
+  const data = {
+    messaging_product: 'whatsapp',
+    to: recipientPhone,
+    type: 'template',
+    template: {
+      name: 'property_tour_event_in_1h',
+      language: {
+        code: 'en',
+      },
+      components: [
+        {
+          type: 'body',
+          parameters: [
+            {
+              type: 'text',
+              text: name,
+            },
+            {
+              type: 'text',
+              text: email,
+            },
+            {
+              type: 'text',
+              text: date,
+            },
+            {
+              type: 'text',
+              text: phoneNumber,
+            },
+            {
+              type: 'text',
+              text: details,
+            },
+          ],
+        },
+      ],
+    },
+  }
+
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    }
+
+    const response = await axios.post(
+      'https://graph.facebook.com/v13.0/307932205726236/messages',
+      data,
+      config
+    )
+
+    // TODO: discuss with @YannyD and fix this
+    // await axios.post(
+    //   `https://us-central1-homeearthnet.cloudfunctions.net/initialMessage`,
+    //   {
+    //     to: recipientPhone,
+    //     message: message,
+    //   }
+    // )
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error("Couldn't send message", error)
+    saveError(error, 'sendMessage')
+    throw error
+  }
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+): Promise<void> {
+  const { template = null } = req.query as { template: string | null }
+  const { recipientPhone, message, name, email } = req.body
+
+  try {
+    createVisitorAndChatRoom(name, email, recipientPhone, message)
+
+    await sendMessage(recipientPhone, message, template)
+  } catch (error) {
+    saveError(error, 'send-whatsapp')
+    return res.status(500).json({ message: "Couldn't send message", error })
+  }
+  return res.status(200).json({ message: 'Message sent' })
+}
