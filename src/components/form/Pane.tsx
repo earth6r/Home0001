5433847bdffe8464557f9fca411b33da1f617f72
James La Marre <jameslamarre@gmail.com>
2024-11-11 15:42:25 -0800

    feat: waitlist update

--- src/components/form/Pane.tsx
+++ src/components/form/Pane.tsx
@@ -48,7 +48,7 @@ const Pane: FC<PaneProps> = ({
       leaveTo="top-1 opacity-0"
       className={classNames(
         className,
-        'flex flex-wrap items-stretch md:items-start gap-y relative w-full h-full'
+        'flex flex-wrap items-stretch md:items-start gap-y relative w-full h-full pb-ydouble'
       )}
     >
       <>
@@ -131,14 +131,7 @@ const Pane: FC<PaneProps> = ({
           </div>
         </div>
 
-        <div
-          className={classNames(
-            block
-              ? 'relative md:w-full bottom-0 xl:bottom-auto'
-              : 'absolute md:w-[calc(50%+4px)] md:ml-auto md:left-[calc(50%-40px)] bottom-[6rem] md:bottom-0',
-            'flex w-full h-btn'
-          )}
-        >
+        <div className={classNames('flex relative w-full h-btn')}>
           {currentStep && currentStep > 0 ? (
             <button
               className="relative flex justify-center items-center w-[48px] h-btn mr-2 bg-white border-black z-above"
