59c8af4a86748048d90b7c4bf038c08f12839e61
apinanyogaratnam <apinanapinan@icloud.com>
2024-11-14 11:56:24 -0500

    refactor: storing userAgent in db

--- src/pages/api/register/save-register.tsx
+++ src/pages/api/register/save-register.tsx
@@ -30,6 +30,7 @@ type RegisterData = {
   locationsOfInterest: Location[]
   buyingTimelinedec2023: BuyingTimeline
   bedroomPreference: BedroomPreference
+  userAgent: string | undefined
 }
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -43,6 +44,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     locationsOfInterest,
     buyingTimelinedec2023,
     bedroomPreference,
+    userAgent = null,
   } = req.body as RegisterData
 
   initializeAdmin() // Initialize Firebase Admin SDK
@@ -86,6 +88,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         ip,
         ipInfo: ipResponse.data,
       },
+      userAgent,
     })
   }
 
