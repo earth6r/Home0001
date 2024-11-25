3979dbd466ebe520e9ca5d081d1788a4c9b0939c
apinanyogaratnam <apinanapinan@icloud.com>
2024-11-25 18:26:38 -0500

    feat: endpoint for instagram leads text message

---
+++ src/pages/api/messages/text-instagram-leads-waitlist.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from 'next/types'
+import { sendTwilioMessage } from './send-twilio-message'
+
+export const config = {
+  maxDuration: 300, // Maximum duration for the API route to respond to a request (5 minutes)
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  const { recipientPhone, message } = req.body
+
+  await sendTwilioMessage(recipientPhone, message, false, true, {
+    template: 'sms-leads-template',
+  })
+  res.status(200).json({ message: 'success' })
+}
