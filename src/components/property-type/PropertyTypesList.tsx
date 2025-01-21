9c1a1cda94756eedd0838c8dc25b3bf4d9f4c41c
James La Marre <jameslamarre@gmail.com>
2025-01-21 12:58:22 -0800

    feat: new property layout

--- src/components/property-type/PropertyTypesList.tsx
+++ src/components/property-type/PropertyTypesList.tsx
@@ -1,90 +1,33 @@
-import { useRef, useState, type FC } from 'react'
+import { type FC } from 'react'
 import classNames from 'classnames'
 import { KeyedPropertyTypeProps, PropertyTypeListProps } from './types'
-
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { SCREENS } from '@/globals'
-import IconRightArrowBold from '@components/icons/IconRightArrowBold'
 import PropertyTypeSummary from './PropertyTypeSummary'
-import { type SwiperOptions } from 'swiper/types'
-import { Navigation } from 'swiper/modules'
 
 export const PropertyTypesList: FC<PropertyTypeListProps> = ({
   propertyTypesList,
   showCity = false,
   className,
 }) => {
-  const slidesRef = useRef(null)
-  const breakpoints: SwiperOptions['breakpoints'] = {
-    0: {
-      slidesPerView: 1.185,
-      centerInsufficientSlides: false,
-    },
-    [SCREENS.md]: {
-      slidesPerView: 'auto',
-      centerInsufficientSlides: true,
-    },
-  }
-  const [activeNav, setActiveNav] = useState(false)
-
   return (
-    <ul
-      className={classNames(className, 'relative')}
-      onMouseOver={() => setActiveNav(true)}
-      onMouseOut={() => setActiveNav(false)}
-    >
-      <Swiper
-        ref={slidesRef}
-        loop={false}
-        spaceBetween={16}
-        breakpoints={breakpoints}
-        modules={[Navigation]}
-        speed={600}
-        navigation={{
-          nextEl: '.swiper-next',
-          prevEl: '.swiper-prev',
-        }}
+    <ul className={classNames(className, 'relative')}>
+      <div
         className={classNames(
-          'relative w-full max-w-full overflow-visible cursor-grab'
+          'grid md:grid-cols-2 gap-xdouble relative w-full max-w-full md:pr-xdouble'
         )}
       >
         {propertyTypesList &&
           propertyTypesList.map((propertyType: KeyedPropertyTypeProps) => {
             return (
-              <SwiperSlide
-                key={propertyType._id}
-                className="w-full md:max-w-[360px]"
-              >
+              <div key={propertyType._id} className="w-full">
                 <PropertyTypeSummary
                   propertyType={propertyType}
                   showCity={showCity}
                   className="w-full"
                 />
-              </SwiperSlide>
+              </div>
             )
           })}
-        <div
-          className={classNames(
-            activeNav ? 'opacity-100' : 'opacity-0',
-            'hidden md:flex md:justify-between absolute w-full top-1/2 transform -translate-y-1/2 transition-opacity duration-200 pointer-events-none z-above'
-          )}
-        >
-          <IconRightArrowBold
-            width="80"
-            fill="black"
-            className={classNames(
-              'pr-xhalf rotate-180 swiper-prev pointer-events-auto cursor-pointer hover:scale-95'
-            )}
-          />
-          <IconRightArrowBold
-            width="80"
-            fill="black"
-            className={classNames(
-              'pr-xhalf relative swiper-next pointer-events-auto cursor-pointer hover:scale-95'
-            )}
-          />
-        </div>
-      </Swiper>
+      </div>
     </ul>
   )
 }
--- src/components/property/Property.tsx
+++ src/components/property/Property.tsx
@@ -4,21 +4,33 @@ import { BlockContent, RichText, SanityMedia } from '@components/sanity'
 import MapDialog from '@components/map/MapDialog'
 import { PropertyElProps } from './types'
 import { PropertyTypesList } from '@components/property-type'
+import IconSmallArrow from '@components/icons/IconSmallArrow'
+import { useWaitlisModal } from '@contexts/modals'
+import { sendGoogleEvent } from '@lib/util'
+import Link from 'next/link'
 
 export const PropertyComponent: FC<PropertyElProps> = ({
   property,
   block,
   className,
 }) => {
+  const [waitlistOpen, setWaitlistOpen] = useWaitlisModal()
+
+  const openWaitlist = () => {
+    setWaitlistOpen(true)
+    const options = { location: window.location.pathname }
+    sendGoogleEvent('opened waitlist modal', options)
+  }
+
   return (
-    <div className={classNames(className)}>
-      <h2 className="mb-ydouble pl-x text-h1 md:text-center pr-menu md:px-x">
-        Home0001: {property?.title}
+    <div className={classNames(className, 'pr-menu')}>
+      <h2 className="absolute w-[100svh] md:w-auto right-0 transform translate-x-[calc(100%-12px)] rotate-90 origin-top-left text-h2">
+        0001 {property?.title}
       </h2>
-      <div className="md:grid md:grid-cols-2 gap-y md:col-start-1 block relative md:pl-x">
-        <div className="flex flex-col md:col-span-1 md:justify-start md:items-start md:sticky top-[64px] left-0 md:h-[64vw] px-x md:px-0">
+      <div className="md:grid md:grid-cols-3 gap-y md:col-start-1 block relative md:pl-x">
+        <div className="flex flex-col md:col-span-1 md:justify-start md:items-start px-x md:px-0">
           {property?.image && (
-            <div className="col-span-2 block relative w-full mb-ydouble md:mb-y z-base">
+            <div className="col-span-2 block relative w-full mb-y z-base">
               <SanityMedia
                 imageProps={{
                   alt: property?.image.alt || 'Building image',
@@ -31,9 +43,34 @@ export const PropertyComponent: FC<PropertyElProps> = ({
               />
             </div>
           )}
+
+          <div className="hidden md:block relative w-full mb-y cursor-pointer z-above">
+            <button
+              onClick={openWaitlist}
+              className={classNames(
+                'w-full relative flex flex-row justify-between items-center h-12 max-h-12 p-x border-black hover:border-white bg-black text-white hover:invert transition-all duration-200 text-button'
+              )}
+            >
+              {`Apply`}
+              <IconSmallArrow width="16" height="10" fill="white" />
+            </button>
+          </div>
+
+          <div className="hidden md:block relative w-full cursor-pointer z-above">
+            <Link href="/how-it-works">
+              <button
+                className={classNames(
+                  'w-full relative flex flex-row justify-between items-center h-12 max-h-12 p-x border-black hover:border-white bg-black text-white hover:invert transition-all duration-200 text-button'
+                )}
+              >
+                {`How it works`}
+                <IconSmallArrow width="16" height="10" fill="white" />
+              </button>
+            </Link>
+          </div>
         </div>
 
-        <div className="col-span-1 overflow-x-hidden">
+        <div className="col-span-1 md:col-span-2 md:overflow-x-hidden">
           <div className="px-x md:px-0 mb-ydouble">
             {property?.header && <RichText blocks={property?.header} />}
 
@@ -50,32 +87,55 @@ export const PropertyComponent: FC<PropertyElProps> = ({
             <BlockContent
               blocks={property?.body}
               grid={false}
-              className="mt-ydouble md:mt-0 pl-x pr-menu md:pl-0 md:pr-x overflow-hidden"
+              className="mt-ydouble md:mt-0 pl-x md:pl-0 pr-x overflow-visible"
             />
           )}
+
+          {!block && (
+            <div className="px-x md:px-0 pt-ydouble mt-ydouble overflow-hidden">
+              {property?.propertyTypesList && (
+                <>
+                  <h2 className="text-h2">Apartments</h2>
+                  {property?.availableText && (
+                    <div className="mt-ydouble uppercase">
+                      {property?.availableText}
+                    </div>
+                  )}
+                  <PropertyTypesList
+                    className="animate-in flex flex-col mt-ydouble"
+                    propertyTypesList={property?.propertyTypesList}
+                  />
+                </>
+              )}
+            </div>
+          )}
+
+          <div className="md:hidden relative w-full mt-header mb-y px-x cursor-pointer z-above">
+            <button
+              onClick={openWaitlist}
+              className={classNames(
+                'w-full relative flex flex-row justify-between items-center h-12 max-h-12 p-x border-black hover:border-white bg-black text-white hover:invert transition-all duration-200 text-button'
+              )}
+            >
+              {`Apply`}
+              <IconSmallArrow width="16" height="10" fill="white" />
+            </button>
+          </div>
+
+          <div className="md:hidden relative w-full px-x cursor-pointer z-above">
+            <Link href="/how-it-works">
+              <button
+                className={classNames(
+                  'w-full relative flex flex-row justify-between items-center h-12 max-h-12 p-x border-black hover:border-white bg-black text-white hover:invert transition-all duration-200 text-button'
+                )}
+              >
+                {`How it works`}
+                <IconSmallArrow width="16" height="10" fill="white" />
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
-
-      {!block && (
-        <div className="pt-ydouble mt-ydouble overflow-hidden">
-          {property?.propertyTypesList && (
-            <>
-              <h2 className="text-h2 md:text-h1 px-x md:text-center">
-                Our Next Releases:
-              </h2>
-              {property?.availableText && (
-                <div className="mt-9 md:px-x uppercase">
-                  {property?.availableText}
-                </div>
-              )}
-              <PropertyTypesList
-                className="animate-in flex flex-col mt-ydouble mx-x"
-                propertyTypesList={property?.propertyTypesList}
-              />
-            </>
-          )}
-        </div>
-      )}
     </div>
   )
 }
