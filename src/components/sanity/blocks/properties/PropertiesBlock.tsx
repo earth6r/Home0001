6ef1ec4d80ef83c0f74422020eb7f59362570b4c
James La Marre <jameslamarre@gmail.com>
2025-02-07 14:05:22 -0800

    feat: updated border

--- src/components/sanity/blocks/properties/PropertiesBlock.tsx
+++ src/components/sanity/blocks/properties/PropertiesBlock.tsx
@@ -23,50 +23,53 @@ export const PropertiesBlock: FC<PropertiesBlockProps> = ({
 
         <div className="flex flex-col pr-x md:pr-0">
           {properties &&
-            properties.map(({ longTitle, slug, available }, index) => (
-              <div
-                key={`property-${slug.current}`}
-                className={classNames(
-                  available === false ? 'opacity-40 pointer-events-none' : '',
-                  'border-bottom--gray last-of-type:border-none'
-                )}
-              >
-                <Link
-                  href={`/property/${slug.current}`}
-                  onClick={() => {
-                    sendGoogleEvent('Click home property tile', {
-                      tileProperty: slug.current,
-                    })
-                    posthog.capture('property_click', {
-                      slug: slug.current,
-                      route: asPath,
-                    })
-                  }}
-                  className="flex justify-between items-center gap-x relative w-full h-[59px]"
-                >
-                  <RichText
-                    blocks={longTitle as TypedObject | TypedObject[]}
-                    className={classNames(
-                      available === false ? '' : 'underline',
-                      'w-[calc(100%-99px-var(--space-x))] uppercase line-clamp-2'
-                    )}
-                  />
-                  {available !== false && (
-                    <div
-                      className={classNames(
-                        'inline-flex justify-between items-center w-[99px] relative px-[6px] pt-[4px] pb-[5px] bg-black text-white font-medium text-left uppercase'
-                      )}
-                    >
-                      <IconRightArrowBold
-                        className="relative w-[1em] mt-[0.1em]"
-                        fill="white"
-                      />
-                      <span className="leading-none">{`Explore`}</span>
-                    </div>
+            properties.map(({ longTitle, slug, available }, index) => {
+              console.log('slug', longTitle, slug)
+              return (
+                <div
+                  key={`property-${slug?.current}`}
+                  className={classNames(
+                    available === false ? 'opacity-40 pointer-events-none' : '',
+                    'border-bottom--gray last-of-type:border-none'
                   )}
-                </Link>
-              </div>
-            ))}
+                >
+                  <Link
+                    href={`/property/${slug?.current}`}
+                    onClick={() => {
+                      sendGoogleEvent('Click home property tile', {
+                        tileProperty: slug.current,
+                      })
+                      posthog.capture('property_click', {
+                        slug: slug.current,
+                        route: asPath,
+                      })
+                    }}
+                    className="flex justify-between items-center gap-x relative w-full h-[59px]"
+                  >
+                    <RichText
+                      blocks={longTitle as TypedObject | TypedObject[]}
+                      className={classNames(
+                        available === false ? '' : 'underline',
+                        'w-[calc(100%-99px-var(--space-x))] uppercase line-clamp-2'
+                      )}
+                    />
+                    {available !== false && (
+                      <div
+                        className={classNames(
+                          'inline-flex justify-between items-center w-[99px] relative px-[6px] pt-[4px] pb-[5px] bg-black text-white font-medium text-left uppercase'
+                        )}
+                      >
+                        <IconRightArrowBold
+                          className="relative w-[1em] mt-[0.1em]"
+                          fill="white"
+                        />
+                        <span className="leading-none">{`Explore`}</span>
+                      </div>
+                    )}
+                  </Link>
+                </div>
+              )
+            })}
         </div>
       </div>
     </Block>
