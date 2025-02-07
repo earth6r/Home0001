9a6fa267189f549485fd15fe4c352b476ede428b
James La Marre <jameslamarre@gmail.com>
2025-02-07 14:05:22 -0800

    feat: updated border

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -142,7 +142,7 @@ export default {
           borderBottom: '1.5px solid black',
         },
         '.border-bottom--gray': {
-          borderBottom: `1.5px solid ${theme('backgroundColor.textgray')}`,
+          borderBottom: `1px solid ${theme('backgroundColor.textgray')}`,
         },
         '.text-h1': {
           fontSize: theme('fontSize.2xl'),
