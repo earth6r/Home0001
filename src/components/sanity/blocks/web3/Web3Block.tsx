916e371fa9fcaa7f3998381e49ce9782b3c8b59d
James La Marre <jameslamarre@gmail.com>
2025-04-28 13:22:15 -0700

    feat: utils for web3 actions

--- src/components/sanity/blocks/web3/Web3Block.tsx
+++ src/components/sanity/blocks/web3/Web3Block.tsx
@@ -7,12 +7,18 @@ import { SanityBlockElement } from '@components/sanity/types'
 import type { Web3Block as Web3BlockType } from '@gen/sanity-schema'
 import classNames from 'classnames'
 import { RichText } from '@components/sanity/rich-text'
-import { createWalletClient, custom, createPublicClient, http } from 'viem'
+import { createWalletClient, custom } from 'viem'
 import { sepolia } from 'viem/chains'
 import { token } from '@studio/lib/sanity.fetch'
 import IconSmallArrow from '@components/icons/IconSmallArrow'
 import Link from 'next/link'
 import { TypedObject } from 'sanity'
+import {
+  fetchImageUrl,
+  fetchTokenURI,
+  mintToken,
+  getTokensOwnedByAddress,
+} from '@lib/util/web3'
 
 declare global {
   interface Window {
@@ -23,74 +29,6 @@ declare global {
 type Web3BlockProps = Omit<SanityBlockElement, keyof Web3BlockType> &
   Web3BlockType
 
-//need to move to library
-const CONTRACT_ADDRESS = '0xa37D0EbC70A41b51c5f6cbdD4D6E646dB3D690d3'
-
-//move to library
-const ABI = [
-  {
-    constant: true,
-    inputs: [{ name: 'tokenId', type: 'uint256' }],
-    name: 'tokenURI',
-    outputs: [{ name: '', type: 'string' }],
-    type: 'function',
-  },
-  {
-    name: 'balanceOf',
-    type: 'function',
-    stateMutability: 'view',
-    inputs: [{ name: 'owner', type: 'address' }],
-    outputs: [{ name: 'balance', type: 'uint256' }],
-  },
-  {
-    name: 'tokenOfOwnerByIndex',
-    type: 'function',
-    stateMutability: 'view',
-    inputs: [
-      { name: 'owner', type: 'address' },
-      { name: 'index', type: 'uint256' },
-    ],
-    outputs: [{ name: 'tokenId', type: 'uint256' }],
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'to',
-        type: 'address',
-      },
-    ],
-    name: 'mint',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-]
-
-//move to web3 utility -  we might want a utils specifically for the NFT
-async function fetchTokenURI(tokenId: number) {
-  const client = createPublicClient({
-    chain: sepolia,
-    transport: http(),
-  })
-  const uri = await client.readContract({
-    address: CONTRACT_ADDRESS,
-    abi: ABI,
-    functionName: 'tokenURI',
-    args: [tokenId],
-  })
-  return uri
-}
-
-//see above note
-async function fetchImageUrl(uri: string): Promise<string> {
-  const ipfsUrl = uri.replace('ipfs://', 'https://ipfs.io/ipfs/')
-  const response = await fetch(ipfsUrl)
-  const metadata = await response.json()
-  return metadata.image.replace('ipfs://', 'https://ipfs.io/ipfs/')
-}
-
-//toDO: fix this type error
 export const Web3Block: FC<Web3BlockProps> = ({
   className,
   header,
@@ -150,63 +88,12 @@ export const Web3Block: FC<Web3BlockProps> = ({
         setAddress(null)
       }
     }
+
     getAddress().catch(err => {
       console.error('Error in getAddress:', err)
     })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [address, isConnected])
-
-  const getToken = async (ownerAddress: string) => {
-    const client = createPublicClient({
-      chain: sepolia,
-      transport: http(),
-    })
-
-    try {
-      const uri = await client.readContract({
-        address: CONTRACT_ADDRESS,
-        abi: ABI,
-        functionName: 'mint',
-        args: [ownerAddress],
-      })
-      console.log('Token URI:', uri)
-      return uri
-    } catch (error) {
-      console.error('Error fetching token URI:', error)
-    }
-  }
-
-  const getTokensOwnedByAddress = async (ownerAddress: any) => {
-    const client = createPublicClient({
-      chain: sepolia,
-      transport: http(),
-    })
-
-    try {
-      const totalSupply = (await client.readContract({
-        address: CONTRACT_ADDRESS,
-        abi: ABI,
-        functionName: 'balanceOf',
-        args: [ownerAddress],
-      })) as number
-      console.log('Total Supply:', totalSupply)
-      const tokenIds = []
-      // Loop over the balance to fetch each token ID owned by the address
-      for (let index = 0; index < totalSupply; index++) {
-        const tokenId = await client.readContract({
-          address: CONTRACT_ADDRESS,
-          abi: ABI,
-          functionName: 'tokenOfOwnerByIndex',
-          args: [ownerAddress, index],
-        })
-        tokenIds.push(tokenId)
-      }
-
-      return tokenIds
-    } catch (error) {
-      console.error('Error fetching tokens:', error)
-    }
-  }
 
   //this button should likely be in a header menu
   const connectWallet = async () => {
@@ -258,7 +145,7 @@ export const Web3Block: FC<Web3BlockProps> = ({
                   <p>No tokens found for this address.</p>
 
                   <button
-                    onClick={() => getToken(address)}
+                    onClick={() => mintToken(address)}
                     className="flex items-center gap-[5px] w-fit py-[4px] px-[6px] border-black bg-white"
                   >
                     <IconSmallArrow fill="black" width="15" height="11" />
