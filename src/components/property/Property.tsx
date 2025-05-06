119d15d197f7c473dd62cd0e70e881894cbef90d
James La Marre <jameslamarre@gmail.com>
2025-05-06 09:44:06 -0700

    feat: type changers

--- src/components/property/Property.tsx
+++ src/components/property/Property.tsx
@@ -46,7 +46,7 @@ export const PropertyComponent: FC<PropertyElProps> = ({
         className={classNames(
           navOpen
             ? 'right-[-16px] lg:right-[calc(-66.666vw+72px)] pb-x bg-white overflow-scroll'
-            : 'right-[calc(-100vw+41px)] lg:right-[calc(-100vw+41px)]',
+            : 'right-[calc(-100vw+44px)] lg:right-[calc(-100vw+44px)]',
           'flex flex-col justify-end gap-8 fixed w-[100svh] h-[calc(100vw+32px)] top-0 pl-header transform translate-x-[calc(100%+16px)] rotate-90 origin-top-left transition-all duration-500 border-none z-above'
         )}
       >
@@ -78,7 +78,7 @@ export const PropertyComponent: FC<PropertyElProps> = ({
             <div
               className={classNames(
                 navOpen ? 'rotate-180' : '',
-                'flex items-center justify-center relative w-[21px] h-[21px] bottom-0 bg-black transition-transform duration-500'
+                'flex items-center justify-center relative w-[21px] h-[21px] bottom-[3px] bg-black transition-transform duration-500'
               )}
             >
               <IconChevron width="12" fill="white" className="rotate-0" />
