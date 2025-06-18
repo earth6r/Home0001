7d9f85bd522c2015f910a2afe405c097369e1087
James La Marre <jameslamarre@gmail.com>
2025-06-18 15:01:17 -0700

    feat: dynamic price wip

---
+++ src/pages/api/web3/get-dynamic-price.ts
@@ -0,0 +1,43 @@
+// Import necessary modules
+import { enableCors } from '@lib/next/cors'
+import { saveError } from '@lib/util/save-error'
+import axios from 'axios'
+
+import { type NextApiRequest, type NextApiResponse } from 'next' // Type definitions for Next.js API routes
+
+// Set configuration options for the API route
+export const config = {
+  maxDuration: 300, // Maximum duration for the API route to respond to a request (5 minutes)
+}
+
+// Handler function to process API requests
+// curl -X GET https://hometrics0001.vercel.app/api/users/get-token-price
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    // Your server-side request (not affected by browser CORS)
+    const response = await axios.get(
+      `https://hometrics0001.vercel.app/api/users/get-token-price`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      }
+    )
+
+    // Return the response from your API
+    console.log('Price fetched successfully:', response.data)
+    return res.status(200).json(response.data)
+  } catch (error: any) {
+    console.error('Error fetching dynamic price:', error.message)
+    saveError(error, 'proxyGetUserProfile')
+
+    // Forward the status code and error
+    return res.status(error.response?.status || 500).json({
+      error: error.response?.data || 'Error fetching dynamic price',
+    })
+  }
+}
+
+// Export the handler as the default export
+export default enableCors(handler)
