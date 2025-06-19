7116b116720b3d50f365cab324a5e4ebb6107e48
James La Marre <jameslamarre@gmail.com>
2025-06-19 13:12:55 -0700

    test: get browser naming

--- src/components/apply/ApplicationPrompt.tsx
+++ src/components/apply/ApplicationPrompt.tsx
@@ -8,56 +8,54 @@ import { isMobile, isIOS, isAndroid, browserName } from 'react-device-detect'
 const ApplicationPrompt: FC<{ header?: string; className?: string }> = ({
   header,
   className,
-}) => (
-  <div className={classNames(className, 'inline-flex flex-col gap-y')}>
-    {`BROWSER NAME: ${browserName}`}
-    <br />
-    {`IS MOBILE: ${isMobile}`}
-    <br />
-    {`IS IOS: ${isIOS}`}
-    <br />
-    {`IS ANDROID: ${isAndroid}`}
-    <br />
-    {`BROWSER NAME (UA): ${navigator.userAgent}`}
+}) => {
+  const isMetamask = navigator.userAgent.includes('MetaMaskMobile')
 
-    {(!isMobile || browserName === 'metamask') && (
-      <>
-        <div className="md:max-w-[50%] rich-text mb-y">
-          <h3 className="text-left mb-ydouble">{header}</h3>
-          <p>To connect your wallet:</p>
-        </div>
+  return (
+    <div className={classNames(className, 'inline-flex flex-col gap-y')}>
+      {(!isMobile || isMetamask) && (
+        <>
+          <div className="md:max-w-[50%] rich-text mb-y">
+            <h3 className="text-left mb-ydouble">{header}</h3>
+            <p>To connect your wallet:</p>
+          </div>
 
-        <WalletButton className="flex items-center gap-[5px] w-fit py-[4px] px-[6px] bg-white border-black">
-          <IconSmallArrow fill="black" width="15" height="11" />
-          <span className="uppercase font-medium leading-none text-xs">
-            {`Connect your wallet`}
-          </span>
-        </WalletButton>
-      </>
-    )}
+          <WalletButton className="flex items-center gap-[5px] w-fit py-[4px] px-[6px] bg-white border-black">
+            <IconSmallArrow fill="black" width="15" height="11" />
+            <span className="uppercase font-medium leading-none text-xs">
+              {`Connect your wallet`}
+            </span>
+          </WalletButton>
+        </>
+      )}
 
-    <div className="rich-text my-y">
-      <span className="text-left">
-        {!isMobile || browserName === 'metamask'
-          ? `Or if you don’t have one already`
-          : `Mobile users please visit our site inside of the metamask mobile app:`}
-      </span>
+      {!isMetamask && (
+        <>
+          <div className="rich-text my-y">
+            <span className="text-left">
+              {!isMobile
+                ? `Or if you don’t have one already`
+                : `Mobile users please visit our site inside of the metamask mobile app:`}
+            </span>
+          </div>
+
+          <Link
+            href={`https://metamask.io/download/`}
+            target="_blank"
+            className="font-bold"
+          >
+            <button className="flex items-center gap-[5px] w-fit py-[4px] px-[6px] bg-white border-black">
+              <IconSmallArrow fill="black" width="15" height="11" />
+
+              <span className="uppercase font-medium leading-none text-xs">
+                {isMobile ? `Get Metamask` : `Create a wallet`}
+              </span>
+            </button>
+          </Link>
+        </>
+      )}
     </div>
-
-    <Link
-      href={`https://metamask.io/download/`}
-      target="_blank"
-      className="font-bold"
-    >
-      <button className="flex items-center gap-[5px] w-fit py-[4px] px-[6px] bg-white border-black">
-        <IconSmallArrow fill="black" width="15" height="11" />
-
-        <span className="uppercase font-medium leading-none text-xs">
-          {isMobile ? `Get Metamask` : `Create a wallet`}
-        </span>
-      </button>
-    </Link>
-  </div>
-)
+  )
+}
 
 export default ApplicationPrompt
