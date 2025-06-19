b0fa29278f8ee4b7f1b8a3e6d8a33696fc79a623
James La Marre <jameslamarre@gmail.com>
2025-06-19 13:05:19 -0700

    test: get browser naming

--- src/components/apply/ApplicationPrompt.tsx
+++ src/components/apply/ApplicationPrompt.tsx
@@ -3,26 +3,45 @@ import classNames from 'classnames'
 import IconSmallArrow from '@components/icons/IconSmallArrow'
 import Link from 'next/link'
 import WalletButton from './WalletButton'
+import { isMobile, isIOS, isAndroid, browserName } from 'react-device-detect'
 
 const ApplicationPrompt: FC<{ header?: string; className?: string }> = ({
   header,
   className,
 }) => (
   <div className={classNames(className, 'inline-flex flex-col gap-y')}>
-    <div className="md:max-w-[50%] rich-text mb-y">
-      <h3 className="text-left mb-ydouble">{header}</h3>
-      <p>To connect your wallet:</p>
-    </div>
+    {`BROWSER NAME: ${browserName}`}
+    <br />
+    {`IS MOBILE: ${isMobile}`}
+    <br />
+    {`IS IOS: ${isIOS}`}
+    <br />
+    {`IS ANDROID: ${isAndroid}`}
+    <br />
+    {`BROWSER NAME (UA): ${navigator.userAgent}`}
 
-    <WalletButton className="flex items-center gap-[5px] w-fit py-[4px] px-[6px] bg-white border-black">
-      <IconSmallArrow fill="black" width="15" height="11" />
-      <span className="uppercase font-medium leading-none text-xs">
-        {`Connect your wallet`}
-      </span>
-    </WalletButton>
+    {(!isMobile || browserName === 'metamask') && (
+      <>
+        <div className="md:max-w-[50%] rich-text mb-y">
+          <h3 className="text-left mb-ydouble">{header}</h3>
+          <p>To connect your wallet:</p>
+        </div>
+
+        <WalletButton className="flex items-center gap-[5px] w-fit py-[4px] px-[6px] bg-white border-black">
+          <IconSmallArrow fill="black" width="15" height="11" />
+          <span className="uppercase font-medium leading-none text-xs">
+            {`Connect your wallet`}
+          </span>
+        </WalletButton>
+      </>
+    )}
 
     <div className="rich-text my-y">
-      <span className="text-left">Or if you don’t have one already:</span>
+      <span className="text-left">
+        {!isMobile || browserName === 'metamask'
+          ? `Or if you don’t have one already`
+          : `Mobile users please visit our site inside of the metamask mobile app:`}
+      </span>
     </div>
 
     <Link
@@ -34,7 +53,7 @@ const ApplicationPrompt: FC<{ header?: string; className?: string }> = ({
         <IconSmallArrow fill="black" width="15" height="11" />
 
         <span className="uppercase font-medium leading-none text-xs">
-          {`Create a wallet`}
+          {isMobile ? `Get Metamask` : `Create a wallet`}
         </span>
       </button>
     </Link>
